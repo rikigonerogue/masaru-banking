09c15c512be9f24b8ee200ab8685d11dedcae619
rikigonerogue <malmbergfrederik@gmail.com>
2024-05-12 10:47:06 +0300

    initial project structure

---
+++ back-end/src/main/java/com/sda/masarubanking/MasarubankingApplication.java
@@ -0,0 +1,13 @@
+package com.sda.masarubanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasarubankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasarubankingApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/sda/masarubanking/MasarubankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.masarubanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasarubankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
