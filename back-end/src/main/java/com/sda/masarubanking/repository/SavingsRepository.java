a45a860b1b6d934de70d81c6bd14e05a295809e6
rikigonerogue <malmbergfrederik@gmail.com>
2024-06-15 06:22:09 +0300

    basic front-end configuration (still needs a tiny tweak)

--- back-end/src/main/java/com/sda/masarubanking/repository/SavingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sda.masarubanking.repository;
-
-import com.sda.masarubanking.entity.Savings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavingsRepository extends JpaRepository<Savings, Long> {
-}
--- back-end/src/main/java/com/sda/masarubanking/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sda.masarubanking.repository;
-
-import com.sda.masarubanking.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username);
-}
